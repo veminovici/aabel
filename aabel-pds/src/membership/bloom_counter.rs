72a43862cbf4c541f45ac715b066395398bab84e
VLAD EMINOVICI <veminovici@hotmail.com>
2023-03-26 20:55:08 +0300

    Add bloom counter

--- aabel-pds/src/membership/bloom_counter.rs
+++ aabel-pds/src/membership/bloom_counter.rs
@@ -1,20 +1,20 @@
 use crate::{ComputeOptimal, IntoIndexes, RandSeed, WithSeed};
-use bit_vec::BitVec;
 use siphasher::sip128::{Hasher128, SipHasher24};
 use std::{
     hash::{Hash, Hasher},
     marker::PhantomData,
 };
 
-pub struct BloomFilter<T, H = SipHasher24> {
-    bits: BitVec,
+pub struct BloomCounter<T, H = SipHasher24> {
+    counters: Vec<u8>,
+    len: usize,
     m: usize,
     k: usize,
     hasher: H,
     _p: PhantomData<T>,
 }
 
-impl<T, H> BloomFilter<T, H>
+impl<T, H> BloomCounter<T, H>
 where
     H: Copy + Hasher + Hasher128 + WithSeed,
     T: Hash,
@@ -22,13 +22,14 @@ where
     /// Creates a new Bloom filter.
     pub fn new(m: usize, k: usize) -> Self {
         let seed = RandSeed::new();
-        let bits = BitVec::from_elem(m, false);
+        let counters = vec![0; m];
         let hasher = <H as WithSeed>::with_seed(&seed);
 
         Self {
             m,
             k,
-            bits,
+            counters,
+            len: 0,
             hasher,
             _p: PhantomData,
         }
@@ -47,11 +48,6 @@ where
         item.to_indexes(self.m, self.k, self.hasher)
     }
 
-    /// Returns the bytes.
-    pub fn to_bytes(&self) -> Vec<u8> {
-        self.bits.to_bytes()
-    }
-
     /// Returns the number of hash functions used by the filter.
     pub fn hashes(&self) -> usize {
         self.k
@@ -66,14 +62,40 @@ where
     pub fn insert(&mut self, item: &T) {
         self.indexes(item)
             .iter()
-            .for_each(|&idx| self.bits.set(idx, true))
+            .for_each(|&idx| self.counters[idx] = self.counters[idx].saturating_add(1));
+
+        self.len += 1;
     }
 
     /// Determines if an item belongs to the filter.
     pub fn contains(&self, item: &T) -> bool {
-        self.indexes(item)
-            .iter()
-            .all(|&idx| self.bits.get(idx).unwrap())
+        self.indexes(item).iter().all(|&idx| self.counters[idx] > 0)
+    }
+
+    pub fn delete(&mut self, item: &T) {
+        if self.contains(item) {
+            self.indexes(item)
+                .iter()
+                .for_each(|&idx| self.counters[idx] = self.counters[idx].saturating_sub(1));
+
+            self.len -= 1;
+        }
+    }
+
+    pub fn count(&self, item: &T) -> u8 {
+        let mut c = u8::MAX;
+
+        for idx in self.indexes(item) {
+            if self.counters[idx] == 0 {
+                return 0;
+            }
+
+            if self.counters[idx] < c {
+                c = self.counters[idx];
+            }
+        }
+
+        c
     }
 }
 
@@ -84,15 +106,19 @@ mod utests {
 
     #[test]
     fn simple_() {
-        let mut filter = BloomFilter::<usize>::new(100, 10);
+        let mut filter = BloomCounter::<usize>::new(100, 10);
         filter.insert(&10);
+
         let res = filter.contains(&10);
-        assert!(res)
+        assert!(res);
+
+        let c = filter.count(&10);
+        assert_eq!(1, c);
     }
 
     #[quickcheck]
     fn prop_bloom_filter(xs: Vec<usize>) -> bool {
-        let mut filter = BloomFilter::<usize>::new(100000, 10);
+        let mut filter = BloomCounter::<usize>::new(100000, 10);
 
         let _: Vec<_> = xs.iter().inspect(|x| filter.insert(x)).collect();
 
--- aabel-pds/src/membership/mod.rs
+++ aabel-pds/src/membership/mod.rs
@@ -1,6 +1,8 @@
+mod bloom_counter;
 mod bloom_filter;
 mod to_indexes;
 
+pub use bloom_counter::*;
 pub use bloom_filter::*;
 pub use to_indexes::*;
 
