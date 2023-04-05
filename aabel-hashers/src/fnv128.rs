6adee1ef07386df86d604b62af7dc866b7777b37
Vlad Eminovici <veminovici@hotmail.com>
2023-04-05 10:29:57 +0300

    Add fnv128

--- aabel-hashers/src/fnv128.rs
+++ aabel-hashers/src/fnv128.rs
@@ -1,30 +1,30 @@
 use std::hash::{BuildHasherDefault, Hasher};
 
-use const_fnv1a_hash::fnv1a_hash_64;
+use const_fnv1a_hash::fnv1a_hash_128;
 
-pub struct Fnv64Hasher(u64);
+pub struct Fnv128Hasher(u128);
 
-impl Default for Fnv64Hasher {
+impl Default for Fnv128Hasher {
     #[inline]
     fn default() -> Self {
         Self::with_key(0)
     }
 }
 
-impl Fnv64Hasher {
-    pub fn with_key(key: u64) -> Self {
+impl Fnv128Hasher {
+    pub fn with_key(key: u128) -> Self {
         Self(key)
     }
 }
 
-impl Hasher for Fnv64Hasher {
+impl Hasher for Fnv128Hasher {
     fn finish(&self) -> u64 {
-        self.0
+        (self.0 >> 64) as u64
     }
 
     fn write(&mut self, bytes: &[u8]) {
         let Self(mut hash) = *self;
-        let h = fnv1a_hash_64(bytes, None);
+        let h = fnv1a_hash_128(bytes, None);
 
         hash ^= h;
         hash = hash.wrapping_mul(0x100000001b3);
@@ -33,26 +33,26 @@ impl Hasher for Fnv64Hasher {
     }
 }
 
-/// A builder for default FNV-64 hashers.
-pub type Fnv64BuildHasher = BuildHasherDefault<Fnv64Hasher>;
+/// A builder for default FNV-128 hashers.
+pub type Fnv128BuildHasher = BuildHasherDefault<Fnv128Hasher>;
 
 // #[cfg(feature = "std")]
 use std::collections::{HashMap, HashSet};
 
 /// A `HashMap` using a default FNV-64 hasher.
 // #[cfg(feature = "std")]
-pub type Fnv64HashMap<K, V> = HashMap<K, V, Fnv64BuildHasher>;
+pub type Fnv128HashMap<K, V> = HashMap<K, V, Fnv128BuildHasher>;
 
 /// A `HashSet1 using a default FNV-64 hasher.
 // #[cfg(feature = "std")]
-pub type Fnv64HashSet<T> = HashSet<T, Fnv64BuildHasher>;
+pub type Fnv128HashSet<T> = HashSet<T, Fnv128BuildHasher>;
 
 #[cfg(test)]
 mod utests {
     use super::*;
 
     fn fnv1a(bytes: &[u8]) -> u64 {
-        let mut hasher = Fnv64Hasher::default();
+        let mut hasher = Fnv128Hasher::default();
         hasher.write(bytes);
         hasher.finish()
     }
--- aabel-hashers/src/lib.rs
+++ aabel-hashers/src/lib.rs
@@ -1,3 +1,5 @@
+mod fnv128;
 mod fnv64;
 
+pub use fnv128::*;
 pub use fnv64::*;
