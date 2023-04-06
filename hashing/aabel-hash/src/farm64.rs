ecb2ff8ce84db63da2aaaefd8313b026ecf90201
Vlad Eminovici <veminovici@hotmail.com>
2023-04-06 14:39:34 +0300

    Add farm64

---
+++ hashing/aabel-hash/src/farm64.rs
@@ -0,0 +1,17 @@
+use std::hash::Hasher;
+use farmhash::hash64;
+
+#[derive(Default)]
+pub struct Farm64Hasher {
+    bytes: Vec<u8>,
+}
+
+impl Hasher for Farm64Hasher {
+    fn finish(&self) -> u64 {
+        hash64(&self.bytes)
+    }
+
+    fn write(&mut self, bytes: &[u8]) {
+        self.bytes.extend_from_slice(bytes)
+    }
+}
--- hashing/aabel-hash/src/lib.rs
+++ hashing/aabel-hash/src/lib.rs
@@ -1,5 +1,11 @@
+#[cfg(feature = "farm")]
+mod farm64;
+
 mod hash_ext;
 
 pub mod hash {
+    #[cfg(feature = "farm")]
+    pub use crate::farm64::*;
+    
     pub use crate::hash_ext::*;
 }
