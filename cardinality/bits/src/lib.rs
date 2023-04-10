a03fce618b09193668529c8892aa40801a06f474
Vlad Eminovici <veminovici@hotmail.com>
2023-04-10 22:41:42 +0300

    Clean the code

--- cardinality/bits/src/lib.rs
+++ cardinality/bits/src/lib.rs
@@ -1,13 +1,17 @@
-use std::{mem::size_of, fmt::Binary, ops::{BitAnd, BitOrAssign, BitAndAssign, ShrAssign}};
+use std::{
+    fmt::Binary,
+    mem::size_of,
+    ops::{BitAnd, BitAndAssign, BitOrAssign, ShrAssign},
+};
 
 use byteorder::{ByteOrder, LittleEndian};
 
-pub trait IntoBool {
-    fn into_bool(&self) -> bool;
+pub trait ToBool {
+    fn to_bool(&self) -> bool;
 }
 
-impl IntoBool for u8 {
-    fn into_bool(&self) -> bool {
+impl ToBool for u8 {
+    fn to_bool(&self) -> bool {
         self != &0
     }
 }
@@ -32,9 +36,18 @@ impl LastBit for u8 {
     }
 }
 
-pub trait MyBits<const N: usize>
-{
-    type Inner: 'static + Copy +  Clone + Binary + BitOrAssign + BitAndAssign + BitAnd<Output = Self::Inner> + IntoBool + Zero + ShrAssign<usize> + LastBit;
+pub trait MyBits<const N: usize> {
+    type Inner: 'static
+        + Copy
+        + Clone
+        + Binary
+        + BitOrAssign
+        + BitAndAssign
+        + BitAnd<Output = Self::Inner>
+        + ToBool
+        + Zero
+        + ShrAssign<usize>
+        + LastBit;
 
     const SET_MASKS: &'static [Self::Inner];
     const RESET_MASKS: &'static [Self::Inner];
@@ -67,7 +80,7 @@ pub trait MyBits<const N: usize>
     fn get(&self, slot: usize, offset: u8) -> bool {
         let mask = Self::SET_MASKS[offset as usize];
         let v = self.get_slot(slot) & mask;
-        v.into_bool()
+        v.to_bool()
     }
 
     fn set(&mut self, slot: usize, offset: u8) {
@@ -83,10 +96,9 @@ pub trait MyBits<const N: usize>
     }
 
     fn merge_value(&mut self, start_slot: usize, value: &[Self::Inner]) {
-
-        for i in 0..value.len() {
+        for (i, item) in value.iter().enumerate() {
             let slot = self.get_slot_mut(start_slot + i);
-            *slot |= value[i];
+            *slot |= *item;
         }
     }
 
@@ -101,7 +113,7 @@ pub trait MyBits<const N: usize>
                 x >>= j;
 
                 if x.is_last_bit_one() {
-                    return (i * 8 + j) as usize;
+                    return i * 8 + j;
                 }
             }
         }
@@ -142,9 +154,7 @@ impl<const N: usize> MyBits<N> for MyBits8<N> {
     .as_slice();
 
     fn from_value(bits: [Self::Inner; N]) -> Self {
-        Self {
-            bits
-        }
+        Self { bits }
     }
 
     fn get_slot(&self, slot: usize) -> Self::Inner {
@@ -152,17 +162,13 @@ impl<const N: usize> MyBits<N> for MyBits8<N> {
     }
 
     fn get_slot_mut(&mut self, slot: usize) -> &mut Self::Inner {
-        let xs = &mut self.bits[slot];
-        xs
+        &mut self.bits[slot]
     }
-
 }
 
 impl<const N: usize> MyBits8<N> {
     pub fn new() -> Self {
-        Self {
-            bits: [0; N]
-        }
+        Self { bits: [0; N] }
     }
 
     pub fn merge_u16(&mut self, start_slot: usize, value: u16) {
@@ -182,7 +188,7 @@ impl<const N: usize> MyBits8<N> {
             for j in 0..8 {
                 x >>= j;
                 if (x & 1) == 1 {
-                    return (i * 8 + j) as usize;
+                    return i * 8 + j;
                 }
             }
         }
@@ -191,94 +197,9 @@ impl<const N: usize> MyBits8<N> {
     }
 }
 
-pub struct Bits<const N: usize> {
-    bits: [u8; N],
-}
-
-impl<const N: usize> Bits<N> {
-    const SET_MASKS: &'static [u8] = [
-        0b0000_0001,
-        0b0000_0010,
-        0b0000_0100,
-        0b0000_1000,
-        0b0001_0000,
-        0b0010_0000,
-        0b0100_0000,
-        0b1000_0000,
-    ]
-    .as_slice();
-
-    const RESET_MASKS: &'static [u8] = [
-        0b1111_1110,
-        0b1111_1101,
-        0b1111_1011,
-        0b1111_0111,
-        0b1110_1111,
-        0b1101_1111,
-        0b1011_1111,
-        0b0111_1111,
-    ]
-    .as_slice();
-
-    pub fn new() -> Self {
-        Self { bits: [0; N] }
-    }
-
-    pub fn from_value(xs: [u8; N]) -> Self {
-        Self { bits: xs }
-    }
-
-    pub fn pretty(&self) -> String {
-        (0..N).fold("".to_owned(), |acc, i| {
-            if i == 0 {
-                format!("{:08b}", self.bits[i])
-            } else {
-                format!("{acc} {:08b}", self.bits[i])
-            }
-        })
-    }
-
-    /// Returns the total number of bits that can be stored.
-    pub fn bits(&self) -> usize {
-        N * size_of::<u8>() * 8
-    }
-
-    /// Returns the total number of `u8` slots.
-    pub fn slots(&self) -> usize {
-        N
-    }
-
-    /// Returns the bit value from a givem position.
-    pub fn get(&self, slot: usize, offset: u8) -> bool {
-        let mask = Self::SET_MASKS[offset as usize];
-        self.bits[slot] & mask != 0
-    }
-
-    /// Sets to 1 the bit value on a given position.
-    pub fn set(&mut self, slot: usize, offset: u8) {
-        let mask = Self::SET_MASKS[offset as usize];
-        self.bits[slot] |= mask;
-    }
-
-    /// Resets to 0 the bit value on a given position.
-    pub fn reset(&mut self, slot: usize, offset: u8) {
-        let mask = Self::RESET_MASKS[offset as usize];
-        self.bits[slot] &= mask;
-    }
-
-    pub fn merge_u16(&mut self, start_slot: usize, value: u16) {
-        let mut ltl = [0u8; 2];
-        LittleEndian::write_u16(&mut ltl, value);
-
-        for i in 0..2 {
-            self.bits[start_slot + i] |= ltl[i];
-        }
-    }
-}
-
-impl<const N: usize> From<[u8; N]> for Bits<N> {
-    fn from(value: [u8; N]) -> Self {
-        Self::from_value(value)
+impl<const N: usize> Default for MyBits8<N> {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
