10587fd4c2c54150e5ad8b714c7bd57a74656013
Vlad Eminovici <veminovici@hotmail.com>
2023-04-14 15:42:10 +0300

    Add more tests

--- sketches/sketch-hashing/src/hasher_ext.rs
+++ sketches/sketch-hashing/src/hasher_ext.rs
@@ -14,9 +14,49 @@ impl<H: Hasher> HasherExt for H {
 
 #[cfg(test)]
 mod utests {
+    use std::hash::Hash;
+
     use crate::{random, UniversalHasher};
 
     use super::*;
+
+    struct MyHasher {
+        bytes: Vec<u8>,
+        k: u64,
+        q: u64,
+        p: u64,
+    }
+
+    impl MyHasher {
+        fn new(k: u64, q: u64, p: u64) -> Self {
+            Self {
+                bytes: vec![],
+                k,
+                q,
+                p,
+            }
+        }
+
+        pub fn get_hash<T: Hash>(k: u64, q: u64, p: u64, item: T) -> u64 {
+            let mut me = Self::new(k, q, p);
+            item.hash64(&mut me)
+        }
+    }
+
+    impl Hasher for MyHasher {
+        fn finish(&self) -> u64 {
+            let mut h = 0u64;
+            self.bytes.iter().enumerate().for_each(|(i, b)| {
+                h += (*b as u64) << i * 8;
+            });
+
+            (self.k * h + self.q) % self.p
+        }
+
+        fn write(&mut self, bytes: &[u8]) {
+            self.bytes.extend(bytes);
+        }
+    }
 
     #[test]
     fn simple_() {
@@ -29,5 +69,26 @@ mod utests {
 
         assert_eq!(n, indexes.len());
         assert!(indexes.iter().all(|i| i < &n));
+    }
+
+    #[test]
+    fn myhasher_() {
+        let h1: Vec<_> = (0..19)
+            .map(|i| MyHasher::get_hash(22, 5, 31, i) % 19)
+            .collect();
+        let h2: Vec<_> = (0..19)
+            .map(|i| MyHasher::get_hash(30, 2, 31, i) % 19)
+            .collect();
+        let h3: Vec<_> = (0..19)
+            .map(|i| MyHasher::get_hash(21, 23, 31, i) % 19)
+            .collect();
+        let h4: Vec<_> = (0..19)
+            .map(|i| MyHasher::get_hash(15, 6, 31, i) % 19)
+            .collect();
+
+        println!("h1: {h1:?}");
+        println!("h2: {h2:?}");
+        println!("h3: {h3:?}");
+        println!("h4: {h4:?}");
     }
 }
